5889ebbe81cd01236ad4cf6e4c1125b07bf2c725
Lucas Andrade <lucasandradeunb@gmail.com>
2016-07-18 12:06:41 -0300

    program is done. And work very well using Lotomania lottery game

--- lotery.cpp
+++ lotery.cpp
@@ -1,15 +1,19 @@
 /*
+
 Name: Lucas Andrade Olivera
 University: UnB - Campus Gama
 Age: 20 years
 Country: Brasil
 Program Description: This program show how many times some 
 number has appeared in specific lottery game of Brasil.
+Operational System LINUX - UBUNTU 15.04
+
 */
 
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #define numbersGame 100
 using namespace std;
 
@@ -27,19 +31,26 @@ typedef struct {
 // list of functions
 void setValueNumbers(Info *lot);
 int countLines();
+string getGameName();
 void getNumbersInfo(Info *lot, int linesCount);
-void showInfo(Info *lot);
+void orderByCount(Info* lot);
+void putInfoInFile(Info* lot, int linesCount, string gameName);
+void showInfo(Info *lot, int linesCount);
 
 // function main begin here
 int main() {
  
 	int lines=0;
   lines = countLines();
-  cout << "Lines = " << lines << endl;
+  string nameGame;
+  nameGame = getGameName();
   Info *lottery = (Info *) malloc(sizeof(Info)*numbersGame);
   setValueNumbers(lottery);
 	getNumbersInfo(lottery, lines);
-	showInfo(lottery);
+	orderByCount(lottery);
+	putInfoInFile(lottery, lines, nameGame);
+	cout << "Para ver o resultado basta abrir o arquivo dataFile.txt para visualizar os dados\n";
+	//showInfo(lottery, lines);
 	return 0;
 
 }
@@ -70,6 +81,15 @@ void setValueNumbers(Info *lot) {
 	}
 }
 
+// get the game name of lottery
+string getGameName() {
+	
+	string gameName;
+	cout << "Digite o nome do jogo que deseja analisar\n";
+	cin >> gameName;
+	return gameName;
+}
+
 // get count and percent of all numbers game
 void getNumbersInfo(Info *lot, int linesCount) {
 
@@ -82,10 +102,46 @@ void getNumbersInfo(Info *lot, int linesCount) {
 	fclose(results);
 }
 
+// order struct INFO by count
+void orderByCount(Info* lot) {
+
+	int i=0, j=0;
+	Info helper;
+	for(i=numbersGame-1; i >= 1; i--) { 
+    for(j=0; j < i ; j++) { 
+      if(lot[j].count>lot[j+1].count) { 
+        helper = lot[j];
+        lot[j] = lot[j+1];
+        lot[j+1] = helper;
+      }
+    }
+	}
+}
+
+// put Info of numbers in some file
+void putInfoInFile(Info* lot, int linesCount, string gameName) {
+
+	infoResults = fopen("dataFile.txt", "w+");
+	fprintf(infoResults, "Resultado da %s.\n\n", gameName.c_str());
+	fprintf(infoResults, "Esse arquivo analisa %d resultados da %s.\n", linesCount, gameName.c_str());
+	fprintf(infoResults, "Mostrando o número, a quantidade de vezes que ele aparece nos %d jogos e também a porcentagem\n\n", linesCount);
+	int i=0;
+	for(i=0; i<numbersGame; i++) {
+		if (lot[i].number<10) {
+			fprintf(infoResults, "Número: %d   Quantidade: %d  Porcentagem: %.03lf%%\n", lot[i].number, lot[i].count, lot[i].percent);
+		}
+		else {
+			fprintf(infoResults, "Número: %d  Quantidade: %d  Porcentagem: %.03lf%%\n", lot[i].number, lot[i].count, lot[i].percent);
+		}
+	}
+	fclose(infoResults);
+}
+
 // show the numbers and their information
-void showInfo(Info *lot) {
+void showInfo(Info *lot, int linesCount) {
 	
 	int i=0;
+	cout << "Lines = " << linesCount << endl;
 	for(i=0; i<numbersGame; i++) {
 		printf ("Number: %d  Times: %d  Percent: %.03lf\n", lot[i].number, lot[i].count, lot[i].percent);
 	}
